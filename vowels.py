a7ba03ca3c2a79e179469244e5fe476fb8b08567
D. F <ifonei@hotmail.com>
2024-06-23 11:58:11 +0300

    adding numbers multiplication

--- vowels.py
+++ vowels.py
@@ -5,3 +5,12 @@ vowelsFound = filter(lambda char: char.lower() in vowels, str)
 vowelsList = list(vowelsFound)
 print(len(vowelsList))
 
+
+
+
+
+numbers = [40,35, 10, 15, 20]
+def multply(num):
+    return num * num
+numbersMultiplication = map(multply,numbers)
+print(list(numbersMultiplication))
