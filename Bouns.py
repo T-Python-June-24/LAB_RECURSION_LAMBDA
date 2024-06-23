6f9da7192346a29aff3fb73fc48626197fe9c7ff
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-06-23 14:26:32 +0300

    bouns lab

--- Bouns.py
+++ Bouns.py
@@ -1,13 +1,13 @@
-# for num in range(1,100):
-#     stringValue = " "
-#     if num % 3 == 0:
-#         stringValue += "Fizz"
-#     if num % 5 == 0: 
-#         stringValue += "Buzz"
-#     if num % 3 != 0 and num % 5 != 0 :
-#         stringValue += str(num)
+for num in range(1,100):
+    stringValue = " "
+    if num % 3 == 0:
+        stringValue += "Fizz"
+    if num % 5 == 0: 
+        stringValue += "Buzz"
+    if num % 3 != 0 and num % 5 != 0 :
+        stringValue += str(num)
 
-#     print(stringValue)
+    print(stringValue)
 
 num = 1
 while  num <= 100:
