ae8585ce5e8470fcba6b950f0eb77fe373533be2
Ahad Fahad <ahadqw@hotmail.com>
2024-06-23 11:47:05 +0300

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,26 @@
+def FB_for():
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+def FB_while():
+    i=1
+    while i<=100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)   
+        i += 1
+
+FB_for()
+FB_while()
