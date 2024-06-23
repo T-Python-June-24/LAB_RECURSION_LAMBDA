5bc66c8ffde8211800e8bfd354b9cfd7f9bf5329
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-06-23 13:32:43 +0300

    implement lambda fuction

--- Program.py
+++ Program.py
@@ -1,10 +1,17 @@
-def vowelsCounter(word:str) -> str:
+# def vowelsCounter(word:str) -> str:
     
-    if not word:
-        return 0
-    if word[0].lower() in "aeiou":
-        return 1  + vowelsCounter(word[1:])
-    else:
-        return  vowelsCounter(word[1:])
+#     if not word:
+#         return 0
+#     if word[0].lower() in "aeiou":
+#         return 1  + vowelsCounter(word[1:])
+#     else:
+#         return  vowelsCounter(word[1:])
 
-print(vowelsCounter("I love python"))
+# print(vowelsCounter("I love python"))
+
+
+listOfNumber:int = [40,35, 10, 15, 20]
+
+multiplyedList:int = list(map(lambda x: x * x, listOfNumber))
+
+print(multiplyedList)
